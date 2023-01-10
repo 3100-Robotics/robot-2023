3395b4844d3fceae80ed3d85ca3ff6930902c63d
sammypants37 <samdlarson@outlook.com>
2023-01-10 17:13:58 -0600

    fixing constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,15 +23,30 @@ public final class Constants {
     public static final int frontRightPort = 2;
     public static final int backLeftPort = 3;
     public static final int backRightPort = 4;
+
+    public final static double kSensorUnitsPerRotation = 2048;
+    private final static double kGearReduction = 8.68;
+    private final static double driveWheelRadiusMeters = 0.0508;
+
+    public final static double kp = 0;
+    public final static double ki = 0;
+    public final static double kd = 0;
+    public final static double kDriveToleranceMeter = 0.1;
+    public final static double kDriveRateToleranceMeterPerS = 0.2;
+
+    public static double encoderScale = (1 / kGearReduction) * (kSensorUnitsPerRotation)
+        * (2 * Math.PI * driveWheelRadiusMeters);
   }
 
   public static class ArmMotorConstants{
     public static final int armMotor1 = 5;
-  }
+    public static final int armMotor2 = 6;
 
-  public static class ElevatorConstants{
-    public static final int elevatorMotor1 = 6;
-    public static final int elevatorMotor2 = 7;
+    public final static double kp = 0;
+    public final static double ki = 0;
+    public final static double kd = 0;
+
+    // public final static double floorLength
   }
 
 }
