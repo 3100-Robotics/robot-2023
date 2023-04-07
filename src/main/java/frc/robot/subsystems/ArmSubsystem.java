419d4b83d7c0baa87979a3b81a7c59cf6ffe98f5
mvog2501 <mvog2501@mpsedu.org>
2023-04-07 02:24:31 -0500

    Added comments for clarification

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
+    // Arm viewer
     private final Mechanism2d mech = new Mechanism2d(3, 2); // Create a simulated workspace 2x3 meters
     private final MechanismRoot2d root = mech.getRoot("arm", 1, 0); // Start the arm a little offcenter
 
@@ -15,6 +16,7 @@ public class ArmSubsystem extends SubsystemBase {
     private final MechanismLigament2d forearm = elevator.append(new MechanismLigament2d("forearm", 0, -90));
     
     public ArmSubsystem() {
+        // Publish the arm simulation to SmartDashboard
         SmartDashboard.putData("Arm", mech);
         elevator.setLength(1.5);
         forearm.setLength(0.5);
